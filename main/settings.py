1e43c2f57688ed3070c75a1b040d7790550560da
A-miin <zulaykaisaeva@gmail.com>
2021-08-23 13:39:01 +0600

    Add webapp

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #     custom apps
+    'webapp',
+
+
+    #     lib apps
+
 ]
 
 MIDDLEWARE = [
